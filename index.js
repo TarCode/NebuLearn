648341df7d7ef4589382a660a75c5f241e2876e2
TarCode <taariq@projectcodex.co>
2015-05-06 13:59:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express')
+var app = express()
+var exphbs  = require('express-handlebars')
+
+var galaxies = require('./data/galaxies');
+var planets = require('./data/planets');
+var tech = require('./data/tech');
+var satImg = require('./data/satImg');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('land');
+});
+app.get('/topic', function (req, res) {
+    res.render('topic');
+});
+
+app.get('/galaxies', function (req, res) {
+    res.render('info',{info:galaxies});
+    
+});
+
+app.get('/planets', function (req, res) {
+    res.render('info',{info:planets});
+});
+
+app.get('/tech', function (req, res) {
+    res.render('info',{info:tech});
+});
+
+app.get('/satImg', function (req, res) {
+    res.render('info', {info:satImg});
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
+
