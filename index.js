e9962e67c4bbca27c69670582ca5b57b3bc314a2
TarCode <taariq@projectcodex.co>
2015-05-07 10:22:23 +0200

    changed port

--- index.js
+++ index.js
@@ -49,12 +49,5 @@ app.get('/quiz', function (req, res) {
 
 
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
-
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
